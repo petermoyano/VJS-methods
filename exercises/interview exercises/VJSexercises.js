a99e7e04678abaac8f93ea43e67135ace9108bbf
Peter M <pmoyano@solvd.com>
2023-05-13 10:07:53 -0300

    Reverse words

--- exercises/interview exercises/VJSexercises.js
+++ exercises/interview exercises/VJSexercises.js
@@ -34,6 +34,21 @@ function string1InString2(string1, string2) {
 
 //Write a function that takes an array of objects, and returns an array
 //of the objects name property, only if that property exists
+//[{name: "Peter"},{name: "Sofi", age: 2}{family: "Moians"}] => ["Peter", "Sofi"]
+function giveNames(arr) {
+    let names = [];
+    for (let obj of arr) {
+        if (obj.name !== undefined) names.push(obj.name);
+    }
+    return names;
+}
+console.log(
+    giveNames([
+        { name: "Peter" },
+        { name: "Sofi", age: 2 },
+        { family: "Moians" },
+    ])
+);
 
 //Write a function that prints to the console an n amount of integers with a delay of 1 second between each integer
 function printNumbers(n) {
@@ -48,5 +63,20 @@ function printNumbers(n) {
     }
     if (n > 0) printI(n);
 }
+//printNumbers(5);
 
-printNumbers(5);
+//Given a string, reverse each word in the sentence
+//For example Welcome to this Javascript Guide! should be become emocleW ot siht tpircsavaJ !ediuG
+function revWords(sentence) {
+    const words = sentence.split(" ");
+    const reverseWords = [];
+    for (let word of words) {
+        let reversedWord = [];
+        for (let letter of word) {
+            reversedWord.unshift(letter);
+        }
+        reverseWords.push(reversedWord.join(""));
+    }
+    return reverseWords.join(" ");
+}
+console.log(revWords("Welcome to this Javascript Guide!"));
