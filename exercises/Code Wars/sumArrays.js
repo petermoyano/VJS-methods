4579d7c48aa1dc3e774f3d8ad568a0b267928487
Peter M <pmoyano@solvd.com>
2023-06-20 10:14:31 -0300

    2 exercises

---
+++ exercises/Code Wars/sumArrays.js
@@ -0,0 +1,18 @@
+// Input: [1, 5.2, 4, 0, -1]
+// Output: 9.2
+
+// Input: []
+// Output: 0
+
+// Input: [-2.398]
+// Output: -2.398
+
+function sum(arr) {
+    let result = 0;
+    numbers.forEach((num) => (result += num));
+    return result;
+}
+
+const reverseString = (str) => {
+    let reverseString = 
+}
