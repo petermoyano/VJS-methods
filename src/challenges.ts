9be57649419a1c6b3cf057de66c99925a4657c9a
Peter M <pmoyano@solvd.com>
2023-07-06 10:49:25 -0300

    Db details, DBeaver usage

--- src/challenges.ts
+++ src/challenges.ts
@@ -3,8 +3,12 @@ const DailyGoals = {
     "7/3": "Create, weekly and monthly goals. Make gols and complete one 6 kyu JavaScript challenge. Change responsabilities for achievements in resume.",
     "7/4": "Review the entire express section of the Springboard curriculum",
 };
+const questionsAboutBoca = {
+    ["How many db are there in the proyects? What do we ussually use?"]
+}
 
 const proyects = {
+    "study back end": ["create an ER diagram representing the db"]
     "note taking app": {
         1: "Add a goals tracker (to do app) to the note taking app.",
         2: "Connect back and front end",
@@ -22,6 +26,6 @@ const posibleWeeklyGoals = {
         "Review all example to concepts files",
     ],
     "job search": ["Apply to 15 jobs.", "work on resume"],
-    BE: ["Start with express book"],
+    BE: ["Start with express book", "review SQL joins, left vs right", "Reserarch most common use cases for the DBeaver software"],
     React: ["Read the entire React note section"],
 };
