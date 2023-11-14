8dcf328aef42640c912bd4cf6c2dcd9038b80e72
Peter <pedromoyano454@gmail.com>
2023-11-14 07:00:25 -0300

    prime numbers

--- exercises/ExamplesToConcepts/Python/generators.py
+++ exercises/ExamplesToConcepts/Python/generators.py
@@ -10,6 +10,26 @@ def fractal_sequence():
 
 # Create the generator object
 fractal_object = fractal_sequence()
-for _ in range(1, 11):
+for _ in range(1, 3):
   print(next(fractal_object))
 
+import itertools
+def prime_numbers():
+    """A generator function that generates all prime numbers"""
+    primes_cache = [2]
+    yield 2
+    for current_num in itertools.count(3, 2):
+      is_prime = True
+      for prime in primes_cache:
+        if current_num % prime == 0:
+          is_prime = False
+
+      if is_prime:
+        primes_cache.append(current_num)
+        yield current_num
+
+
+primes_gen = prime_numbers()
+
+for _ in range(1, 21):
+  print(next(primes_gen))
